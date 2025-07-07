315502e84278b47b957472353fc1caa726d3f954
Yvvma <flvckopxdin@gmail.com>
2025-07-07 14:20:17 -0300

    alt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'server', // ✅ Adiciona suporte a rotas de API
+
   vite: {
     plugins: [tailwindcss()]
   },
